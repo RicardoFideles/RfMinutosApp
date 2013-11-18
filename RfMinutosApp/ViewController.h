1d8abdb13421f1493ac933a32710bac12341cd56
Ricardo <ricardo@ricardofideles.com.br>
2013-11-18 11:28:00 -0200

    arquivos iniciais

---
+++ RfMinutosApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RfMinutosApp
+//
+//  Created by Ricardo jorge Fideles junior on 18/11/13.
+//  Copyright (c) 2013 Ricardo jorge Fideles junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RfMinutosAppTests/RfMinutosAppTests.m
@@ -0,0 +1,34 @@
+//
+//  RfMinutosAppTests.m
+//  RfMinutosAppTests
+//
+//  Created by Ricardo jorge Fideles junior on 18/11/13.
+//  Copyright (c) 2013 Ricardo jorge Fideles junior. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RfMinutosAppTests : XCTestCase
+
+@end
+
+@implementation RfMinutosAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
